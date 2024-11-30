d17add417db9a342a3030792ee9643c9ef3c9731
DrakenAAM <andriniaina.2am@gmail.com>
2024-11-30 09:41:08 +0300

    redirection vers home après login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,35 +1,55 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
+    const navigate = useNavigate(); // Hook pour rediriger
 
     const handleLogin = (e) => {
         e.preventDefault();
         axios.post('http://127.0.0.1:8000/api/login/', { username, password })
-        .then((response) => {
-            localStorage.setItem('access', response.data.access);
-            localStorage.setItem('refresh', response.data.refresh);
-            setMessage('Connexion réussie');
-        })
-        .catch((error) => {
-            setMessage("Nom d'utilisateur ou mot de passe incorrect");
-        });
-    }
+            .then((response) => {
+                // Enregistrer les tokens dans le localStorage
+                localStorage.setItem('access', response.data.access);
+                localStorage.setItem('refresh', response.data.refresh);
+                
+                // Afficher un message de succès (facultatif)
+                setMessage('Connexion réussie');
+
+                // Rediriger vers la page home
+                navigate('/home');
+            })
+            .catch((error) => {
+                // Gérer les erreurs d'authentification
+                setMessage("Nom d'utilisateur ou mot de passe incorrect");
+            });
+    };
 
     return (
         <div>
             <h2>Se connecter</h2>
             <form onSubmit={handleLogin}>
-                <input type='text' placeholder="Nom d'utilisateur" value={username} onChange={(e) => setUsername(e.target.value)}/>
-                <input type='password' placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <input 
+                    type='text' 
+                    placeholder="Nom d'utilisateur" 
+                    value={username} 
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input 
+                    type='password' 
+                    placeholder="Mot de passe" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <button type='submit'>Se connecter</button>
             </form>
             {message && <p>{message}</p>}
         </div>
-    )
-}
+    );
+}  
 
-export default Login
+export default Login;
+
