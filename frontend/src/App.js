de3e6c71949f229953edf77bde42c842145adb43
DrakenAAM <andriniaina.2am@gmail.com>
2024-12-15 22:56:58 +0300

    modif App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ProtectedRoute from './pages/ProtectedRoute';
-import ImportFicEmployer from './pages/ImportDataEmployer';
-import ImportFicEmbauche from './pages/ImportDataEmbauche';
-import ImportFicDebauche from './pages/ImportDataDebauche';
+
 
 
 function App() {
@@ -17,9 +15,6 @@ function App() {
       <Routes>
        <Route path='/' element={<Login/>}/>
        <Route path='/register' element={<Register/>}/>
-       <Route path='/ImportEmployer' element={<ImportFicEmployer/>}/>
-       <Route path='/ImportEmbauche' element={<ImportFicEmbauche/>}/>
-       <Route path='/ImportDebauche' element={<ImportFicDebauche/>}/>
        <Route path='/home' 
         element={
           <ProtectedRoute>
